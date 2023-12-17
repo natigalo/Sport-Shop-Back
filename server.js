5d4a6272a034ae7ede331b8245acb34e5f384ce3
natigalo <natigalo1@gmail.com>
2023-12-17 18:42:11 -0500

    port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import not_found_handler from './middlewares/not_found_handler.js';
 import error_handler from './middlewares/error_handler.js';
 
 const server = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
